ab8f6c6f308727e744b0520f882000c756f7ed25
Charlotka R <charlotka.web@gmail.com>
2023-02-01 08:24:16 +0100

    changes

--- src/app/components/exercices-list/exercices-list.component.ts
+++ src/app/components/exercices-list/exercices-list.component.ts
@@ -8,15 +8,38 @@ import { ExerciseService } from 'src/app/services/exercise.service';
 })
 export class ExercicesListComponent implements OnInit {
 
+  apiExercises:any;
+  apiThumbnails:any;
+  exercisesBaseIds:any;
+
   exercises:any;
 
   constructor(private exerciseService:ExerciseService) { }
 
   ngOnInit(): void {
-    this.exerciseService.getPosts() 
-      .subscribe((response: any) => {
-        this.exercises = response['results'];
-      });
+    this.getExercisesApi();
+    this.getThumbnails();
+    this.getExercises();
+  }
+
+  getExercisesApi(): void {
+    this.exerciseService.getExercisesApi() 
+      .subscribe((response: any) => { this.apiExercises = response['results']});
+  }
+
+  getExercises(): void {
+    
+    let data:any = this.getExercisesApi();
+  
+  
+    for (let i = 0; i < data.length; i++ ) {
+      this.exercisesBaseIds.push(data[i]['exercise_base']);
+   }
+  }
+
+  getThumbnails(): void {
+    this.exerciseService.getExerciseImagesApi() 
+      .subscribe((images: any) => { this.apiThumbnails = images['results']});
   }
 
 }
--- src/app/services/exercise.service.ts
+++ src/app/services/exercise.service.ts
@@ -5,12 +5,17 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class ExerciseService {
-  private url = 'https://wger.de/api/v2/exercise/?equipment=7';
+  private apiExercise = 'https://wger.de/api/v2/exercise/?equipment=7&limit=40&language=2';
+  private apiExerciseImages = 'https://wger.de/api/v2/exerciseimage/';
   // private API_KEY = 'b82b208a0dbd9f52b1b03a37e97c901195b59d50';
 
   constructor(private httpClient: HttpClient) { }
 
-  getPosts(){
-    return this.httpClient.get(`https://wger.de/api/v2/exercise/?equipment=7&limit=40&language=2`);
+  getExercisesApi(){
+    return this.httpClient.get(this.apiExercise);
+  }
+
+  getExerciseImagesApi(){
+    return this.httpClient.get(this.apiExerciseImages);
   }
 }
